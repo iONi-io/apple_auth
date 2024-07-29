dba123d11588105c95f9a7eb6a661e6886844057
Chris Hawkins <chrishawkins@hey.com>
2024-07-28 21:39:51 -0400

    Back out "bump deps and support for multiple client IDs"
    Original commit changeset: 2c1d716e7d01

--- lib/apple_auth/helpers/conditions/aud_condition.rb
+++ lib/apple_auth/helpers/conditions/aud_condition.rb
@@ -8,11 +8,7 @@ module AppleAuth
       end
 
       def validate!
-        if AppleAuth.config.apple_client_id.is_a?(Array)
-          return true if AppleAuth.config.apple_client_id.includes(@aud)
-        elsif @aud == AppleAuth.config.apple_client_id
-          return true
-        end
+        return true if @aud == AppleAuth.config.apple_client_id
 
         raise JWTValidationError, 'jwt_aud is different to apple_client_id'
       end
